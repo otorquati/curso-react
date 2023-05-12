1c226b2cd01a091d78677a153cdbfe768557e278
Osvaldo Torquati <62728159+otorquati@users.noreply.github.com>
2023-05-12 00:36:03 -0300

    Starting Course
    1st archive with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React , { useState } from "react";
+
+// Importa o css no React
 import './App.css';
 
-function App() {
+const App = () => {
+  const [ message, setMessage ] =  useState('Olá, mundo!');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <div className="container">{message}</div>;
+    <button onClick={()=>setMessage("Olááááááááááá!")}>Mudar Mensagen</button>  
+  </>
+  )
 }
 
 export default App;
