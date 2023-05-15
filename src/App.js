316c7aefdb83f4272c07e34c3ff756f68abab8ab
Osvaldo <torquati.osvaldo@gmail.com>
2023-05-14 21:51:30 -0300

    update - adding tasks
    developing until position 30:25 of video.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React , { useState } from "react";
+// Import para usar o React
+import React, { useState } from "react";
 
-// Importa o css no React
-import './App.css';
+import Tasks from "./Components/Tasks";
+import "./App.css";
 
 const App = () => {
-  const [ message, setMessage ] =  useState('Olá, mundo!');
+  const [tasks, setTasks]  = useState([
+    {
+      id: "1",
+      title: "Estudar programção",
+      complete: false,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      complete: true,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      complete: true,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      complete: true,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      complete: true,
+    },
+  ]);
+
   return (
   <>
-    <div className="container">{message}</div>;
-    <button onClick={()=>setMessage("Olááááááááááá!")}>Mudar Mensagen</button>  
-  </>
-  )
-}
+    <div className='container'>
+      <Tasks tasks={tasks} />
+    </div>
+      
+    </>
+  );
+};
 
 export default App;
